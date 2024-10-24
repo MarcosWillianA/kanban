372f2b1dce22effede0ffb1b1252e15504059c1c
Marcos Willian <marcoswillianl1987@gmail.com>
2024-10-24 13:46:12 -0400

    terminado (a principio)
    funcionando normal em navegadores, vou testar no celular

---
+++ script/kanban.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll('section');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('dragstart', e => {
+        e.currentTarget.classList.add('arrastavel');
+    })
+
+    card.addEventListener('dragend', e=> {
+        e.currentTarget.classList.remove('arrastavel');
+    })
+})
+
+sections.forEach(secao => {
+    secao.addEventListener('dragover', e => {
+        e.preventDefault();
+        e.currentTarget.classList.add('card-hover');
+    })
+
+    secao.addEventListener('dragleave', e => {
+        e.currentTarget.classList.remove('card-hover');
+    })
+
+    secao.addEventListener('drop', e => {
+        e.currentTarget.classList.remove('card-hover');
+        const arrastado = document.querySelector('.arrastavel');
+        e.currentTarget.appendChild(arrastado);
+    })
+})
